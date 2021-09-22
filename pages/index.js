45657a331a0234257143e3c3c10ee556d26221ab
Gunnlaugur Þór Briem <gunnlaugur@gmail.com>
2021-09-22 16:13:47 +0000

    add hello-wasm-pack and try to call it
    Result: error page with two errors.
    
    1 of 2 unhandled errors:
    
    ```
    Unhandled Runtime Error
    
    ChunkLoadError: Loading chunk node_modules_next_dist_client_dev_noop_js failed.
    (error: http://localhost:3000/_next/static/chunks/fallback/node_modules_next_dist_client_dev_noop_js.js)
    Call Stack
    __webpack_require__.f.j
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/static/chunks/webpack.js (824:29)
    __webpack_require__.e/<
    /_next/static/chunks/webpack.js (229:40)
    __webpack_require__.e
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/static/chunks/webpack.js (228:67)
    createRequire/fn.e
    /_next/static/chunks/webpack.js (463:50)
    <unknown>
    node_modules/next/dist/client/next-dev.js (20:0)
    ./node_modules/next/dist/client/next-dev.js
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/static/chunks/main.js (589:1)
    options.factory
    /_next/static/chunks/webpack.js (774:31)
    __webpack_require__
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/static/chunks/webpack.js (37:33)
    __webpack_exec__
    /_next/static/chunks/main.js (1399:61)
    <unknown>
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/static/chunks/main.js (1400:53)
    <unknown>
    webpackJsonpCallback@http://localhost:3000/_next/static/chunks/webpack.js?ts=1632327165178 (1357:46)
    <unknown>
    /_next/static/chunks/main.js (9:61)
    ```
    
    2 of 2 unhandled errors:
    
    ```
    Server Error
    
    SyntaxError: Cannot use import statement outside a module
    This error happened while generating the page. Any console logs will be displayed in the terminal window.
    Call Stack
    <unknown>
    file:/Users/gthb/git/next-tries-to-do-wasm/node_modules/hello-wasm-pack/hello_wasm_pack.js (2)
    wrapSafe
    internal/modules/cjs/loader.js (988:16)
    Module._compile
    internal/modules/cjs/loader.js (1036:27)
    Object.Module._extensions..js
    internal/modules/cjs/loader.js (1101:10)
    Module.load
    internal/modules/cjs/loader.js (937:32)
    Function.Module._load
    internal/modules/cjs/loader.js (778:12)
    Module.require
    internal/modules/cjs/loader.js (961:19)
    require
    internal/modules/cjs/helpers.js (92:18)
    Object.hello-wasm-pack
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/server/pages/index.js (86:18)
    __webpack_require__
    file:/Users/gthb/git/next-tries-to-do-wasm/.next/server/webpack-runtime.js (33:42)
    eval
    webpack-internal:///./pages/index.js (11:73)
    ```

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { greet } from 'hello-wasm-pack';
 
 export default function Home() {
+  greet();
   return (
     <div className={styles.container}>
       <Head>
