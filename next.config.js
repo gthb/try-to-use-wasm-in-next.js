3dcecd9f39b460247f6920f8031840a43d56326f
Gunnlaugur Þór Briem <gunnlaugur@gmail.com>
2021-09-24 13:38:24 +0000

    set config.output.webassemblyModuleFilename
    ... since that is done in the next.js with-webassembly example:
    https://github.com/vercel/next.js/blob/canary/examples/with-webassembly/next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   experimental: { esmExternals: true },
   webpack: (config, options) => {
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
     config.experiments = {
       asyncWebAssembly: true,
-    }
+    };
     return config;
   },
   reactStrictMode: true,
